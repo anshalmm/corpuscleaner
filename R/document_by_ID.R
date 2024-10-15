f44f3ba363f43950755513c27584309151456f95
anshalm <87345163+anshalm@users.noreply.github.com>
2024-10-15 15:00:51 -0700

    commits

--- R/document_by_ID.R
+++ R/document_by_ID.R
@@ -4,7 +4,6 @@
 #' This function allows you to extract the document of your choice based on the title you choose along with
 #' any extra parameters you'd like to see with it such as the `year` or the `author`.
 #' @return A \code{tibble} with `id`, `text`, and then any column variable of your choice
-#'
 #' @param ... Input any ID (i.e., 1:18) from the `Documents` dataset
 #' @param vars Input any column variable from the `Documents` dataset
 #' @export
