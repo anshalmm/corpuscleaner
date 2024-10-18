dac7a18cc31800a5f64047f4f5b85eb50785d7be
anshalm <87345163+anshalm@users.noreply.github.com>
2024-10-17 18:08:23 -0700

    commits

--- R/unnest_token.R
+++ R/unnest_token.R
@@ -1,6 +1,6 @@
 #' @title unnest_unigrams
 #' @description Tokenizing the text into 1 unit (Word)
-#' @param ... Input in the `id` of your choice from the `Documents` dataset
+#' @param tbl Input in the `tibble` of your choice made from the `document_by_ID` function
 #' @return Splitting text into `1 token`
 #' @format A \code{tibble} with 5 variables:
 #' \describe{
@@ -11,7 +11,6 @@
 #'  \item{word}{Token Column}
 #' }
 #' @export
-#' @importFrom dplyr filter
 #' @importFrom dplyr mutate
 #' @importFrom stringi stri_extract_all_regex
 #' @importFrom tidyr unnest
@@ -21,10 +20,9 @@
 #' @examples
 #' UDocument = document_by_ID(id == 1)
 #' unnest_unigrams(UDocument)
-unnest_unigrams = function(data, args, ...){
+unnest_unigrams = function(data, args){
   args = rlang::enquo(args)
   data %>%
-    filter(...) %>%
     mutate(word = stri_extract_all_regex(text, pattern = "\\b(?:[[:word:]]+)")) %>%
     unnest(word) %>%
     select(id, title, -text, author, year, word)
@@ -33,7 +31,7 @@ unnest_unigrams = function(data, args, ...){
 
 #' @title unnest_bigrams
 #' @description Tokenizing the text into 2 units (Words)
-#' @param ... Input in the `id` of your choice from the `Documents` dataset
+#' @param tbl Input in the `tibble` of your choice made from the `document_by_ID` function
 #' @return Mulitword combination of `2 tokens`
 #'@format A \code{tibble} with 5 variables:
 #' \describe{
@@ -44,7 +42,6 @@ unnest_unigrams = function(data, args, ...){
 #'  \item{token_two}{Token Column}
 #' }
 #' @export
-#' @importFrom dplyr filter
 #' @importFrom dplyr mutate
 #' @importFrom stringi stri_extract_all_regex
 #' @importFrom tidyr unnest
@@ -54,10 +51,9 @@ unnest_unigrams = function(data, args, ...){
 #' @examples
 #' BDocument = document_by_ID(id == 5)
 #' unnest_bigrams(BDocument)
-unnest_bigrams = function(data, args, ...){
+unnest_bigrams = function(data, args){
   args = rlang::enquo(args)
   data %>%
-    filter(...) %>%
     mutate(token_two = stri_extract_all_regex(text, pattern = "\\b(?:[[:word:]]+.[[:word:]]+|[[:word:]]+)")) %>%
     unnest(token_two) %>%
     select(id, title, -text, author, year, token_two)
@@ -66,7 +62,7 @@ unnest_bigrams = function(data, args, ...){
 
 #' @title unnest_trigrams
 #' @description Tokenizing the text into 3 units (Words)
-#' @param ... Input in the `id` of your choice from the `Documents` dataset
+#' @param tbl Input in the `tibble` of your choice made from the `document_by_ID` function
 #' @return Mulitword combination of `3 tokens`
 #' @format A \code{tibble} with 5 variables:
 #' \describe{
@@ -77,7 +73,6 @@ unnest_bigrams = function(data, args, ...){
 #'  \item{token_three}{Token Column}
 #' }
 #' @export
-#' @importFrom dplyr filter
 #' @importFrom dplyr mutate
 #' @importFrom stringi stri_extract_all_regex
 #' @importFrom tidyr unnest
@@ -87,7 +82,7 @@ unnest_bigrams = function(data, args, ...){
 #' @examples
 #' TDocument = document_by_ID(id == 7)
 #' unnest_trigrams(TDocument)
-unnest_trigrams = function(data, args, ...){
+unnest_trigrams = function(data, args){
   args = rlang::enquo(args)
   data %>%
     mutate(token_three = stri_extract_all_regex(text, pattern = "\\b(?:[[:word:]]+.[[:word:]]+.[[:word:]]+)")) %>%
@@ -98,7 +93,7 @@ unnest_trigrams = function(data, args, ...){
 
 #' @title unnest_N4_grams
 #' @description Tokenizing the text into 4 units (Words)
-#' @param ... Input in the `id` of your choice from the `Documents` dataset
+#' @param tbl Input in the `tibble` of your choice made from the `document_by_ID` function
 #' @return Mulitword combination of `4 tokens`
 #' @format A \code{tibble} with 5 variables:
 #' \describe{
@@ -109,7 +104,6 @@ unnest_trigrams = function(data, args, ...){
 #'  \item{token_four}{Token Column}
 #' }
 #' @export
-#' @importFrom dplyr filter
 #' @importFrom dplyr mutate
 #' @importFrom stringi stri_extract_all_regex
 #' @importFrom tidyr unnest
@@ -119,10 +113,9 @@ unnest_trigrams = function(data, args, ...){
 #' @examples
 #' NDocument = document_by_ID(id == 10)
 #' unnest_N4_grams(NDocument)
-unnest_N4_grams = function(data, args, ...){
+unnest_N4_grams = function(data, args){
   args = rlang::enquo(args)
   data %>%
-    filter(...) %>%
     mutate(token_four = stri_extract_all_regex(text, pattern = "\\b(?:[[:word:]]+.[[:word:]]+.[[:word:]]+.[[:word:]]+)")) %>%
     unnest(token_four) %>%
     select(id, title, -text, author, year, token_four)
@@ -130,7 +123,7 @@ unnest_N4_grams = function(data, args, ...){
 
 #' @title unnest_N5_grams
 #' @description Tokenizing the text into 5 units (Words)
-#' @param ... Input in the `id` of your choice from the `Documents` dataset
+#' @param tbl Input in the `tibble` of your choice made from the `document_by_ID` function
 #' @return Mulitword combination of `5 tokens`
 #' @format A \code{tibble} with 5 variables:
 #' \describe{
@@ -141,7 +134,6 @@ unnest_N4_grams = function(data, args, ...){
 #'  \item{token_five}{Token Column}
 #' }
 #' @export
-#' @importFrom dplyr filter
 #' @importFrom dplyr mutate
 #' @importFrom stringi stri_extract_all_regex
 #' @importFrom tidyr unnest
@@ -151,10 +143,9 @@ unnest_N4_grams = function(data, args, ...){
 #' @examples
 #' N5Document = document_by_ID(id == 18)
 #' unnest_N5_grams(N5Document)
-unnest_N5_grams = function(data, args, ...){
+unnest_N5_grams = function(data, args){
   args = rlang::enquo(args)
   data %>%
-    filter(...) %>%
     mutate(token_five = stri_extract_all_regex(text, pattern = "\\b(?:[[:word:]]+.[[:word:]]+.[[:word:]]+.[[:word:]]+.[[:word:]]+)")) %>%
     unnest(token_five) %>%
     select(id, title, -text, author, year, token_five)
